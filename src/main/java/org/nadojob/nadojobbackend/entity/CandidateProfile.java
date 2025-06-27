45c0c546fd825bfa63472c616b992baf06c3705b
AaronDeluna <aaron_deluna@mail.ru>
2025-06-27 13:48:55 +0300

    fix: JK-101 удалил лишний импорт

--- src/main/java/org/nadojob/nadojobbackend/entity/CandidateProfile.java
+++ src/main/java/org/nadojob/nadojobbackend/entity/CandidateProfile.java
@@ -17,7 +17,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
